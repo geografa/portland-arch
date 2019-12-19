e21e92fd25a455c334111ddd9aab477b57aab939
Rafa Gutierrez <rafa@mapbox.com>
2019-12-18 19:15:11 -0800

    adds static map

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ var config = {
             id: 'id-fhd',
             title: 'The Fair-haired Dumb Bell',
             image: './images/pdx/fhd.jpg',
-            description: 'The Fair-Haired Dumbbell is a building located at 11 Northeast Martin Luther King Jr. Blvd., in Portland, Oregon\'s Kerns neighborhood, in the United States. Its exterior design was created by Los Angeles artist James Jean, and was selected by the Regional Arts & Culture Council, the city, and Guerrilla Development. Dan Cohen painted the artwork in June 2017. - Wikipedia, https://en.wikipedia.org/wiki/Fair-Haired_Dumbbell <div id="staticMap"></div>',
+            staticMap: true,
+            description: 'The Fair-Haired Dumbbell is a building located at 11 Northeast Martin Luther King Jr. Blvd., in Portland, Oregon\'s Kerns neighborhood, in the United States. Its exterior design was created by Los Angeles artist James Jean, and was selected by the Regional Arts & Culture Council, the city, and Guerrilla Development. Dan Cohen painted the artwork in June 2017. - Wikipedia, https://en.wikipedia.org/wiki/Fair-Haired_Dumbbell',
             location: {
                 center: [-122.66187, 45.52352],
                 zoom: 17.10,
@@ -37,6 +38,7 @@ var config = {
             id: 'id-kex',
             title: 'KEX',
             image: './images/pdx/kex.jpg',
+            staticMap: true,
             description: 'KEX first started in 2012, when a businessman and a set designer met at an abandoned biscuit factory in a strange corner of Reykjavík while scouting a location for an upcoming film. While the movie never got made, the vision for the set remained.',
             location: {
                 center: [-122.66164, 45.52371],
@@ -61,6 +63,7 @@ var config = {
             id: 'id-ch',
             title: 'US Customs House',
             image: './images/pdx/wework.jpg',
+            staticMap: true,
             description: 'US Customs House | Wework ',
             location: {
                 center: [-122.67761, 45.52458],
@@ -74,6 +77,7 @@ var config = {
             id: 'id-port',
             title: 'The Portland Building',
             image: './images/pdx/portlandia.jpg',
+            staticMap: true,
             description: 'The Portland Building',
             location: {
                 center: [-122.67843, 45.51526],
@@ -88,6 +92,7 @@ var config = {
             id: 'id-vmc',
             title: 'Veterans Memorial Coliseum',
             image: './images/pdx/vmc.jpg',
+            staticMap: true,
             description: 'Veterans Memorial Coliseum',
             location: {
                 center: [-122.66871, 45.53283],
@@ -101,6 +106,7 @@ var config = {
             id: 'id-keller',
             title: 'Keller Fountain Park',
             image: './images/pdx/keller.jpg',
+            staticMap: true,
             description: 'Keller Fountain Park is a city park in downtown Portland, Oregon.[1] Originally named Forecourt Fountain or Auditorium Forecourt, the 0.92-acre (0.37 ha) park opened in 1970 across Third Avenue from what was then Civic Auditorium. In 1978, the park was renamed after Ira C. Keller, head of the Portland Development Commission (PDC) from 1958–1972.[1][2] Civic Auditorium was renamed as Keller Auditorium in 2000, but is named in honor of Ira\'s son, Richard B. Keller. The central feature of the park is the concrete water fountain. Keller Fountain is often noted as a memorable feature of the public landscape in downtown Portland, and in 1999 was awarded a medallion from the American Society of Landscape Architects. The fountain was designed by Angela Danadjieva using inspiration from waterfalls in the Columbia River Gorge located east of Portland. While the park is named Keller Fountain Park, the fountain itself is named Ira Keller Fountain. The fountain\'s pools hold 75,000 US gallons (280,000 l; 62,000 imp gal) of water, while the waterfalls pump 13,000 US gallons (49,000 l; 11,000 imp gal) per minute over the cascade.',
             location: {
                 center: [-122.67889, 45.51248],
@@ -115,6 +121,7 @@ var config = {
             id: 'id-hollywood',
             title: 'Hollywood Theater',
             image: './images/pdx/hollywood.jpg',
+            staticMap: true,
             description: 'Hollywood Theater',
             location: {
                 center: [-122.62082, 45.53567],
